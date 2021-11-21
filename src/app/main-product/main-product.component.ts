41b57c2daabadf70570fc1782cdfb348505bbe1c
ShaymaREBHI <shayma.rebhi@esprit.tn>
2021-11-21 15:52:36 +0100

    productCrud

--- src/app/main-product/main-product.component.ts
+++ src/app/main-product/main-product.component.ts
@@ -7,8 +7,10 @@ import { product } from '../model/product';
   styleUrls: ['./main-product.component.css']
 })
 export class MainProductComponent implements OnInit {
-listProduct : product[];
-showFormTemplate: boolean;
+  listProduct: product[];
+  showFormTemplate: boolean;
+  buttonValue: string;
+  inputProduct: product
 
   constructor() { }
 
@@ -48,26 +50,44 @@ showFormTemplate: boolean;
       }
     ]
   }
-  like(product:product){
+
+
+  like(product: product): void{
     let i = this.listProduct.indexOf(product);
     this.listProduct[i].nbrLike++
-    console.log(this.listProduct)
+    //console.log(this.listProduct)
+  }
+  //the method save will check if is an update case or adding a new product case
+  save(product: product): void{
+    let i = this.listProduct.indexOf(product);
+    if(i!= -1){
+      this.listProduct[i]= product
     }
-
-    save(product: product){
-      this.listProduct.push(product);
+    else this.listProduct.push(product);
+    this.showFormTemplate = false
+  }
+  showForm(){
+    if (this.showFormTemplate ===false){
+      this.showFormTemplate = true
+      this.buttonValue= "go Back to the List";
+      this.inputProduct = new product();
+    }
+    else {
+      this.buttonValue="add new Product";
       this.showFormTemplate = false
+    }
+  }
+  delete(product:product): void{
+    let i = this.listProduct.indexOf(product);
+    this.listProduct.splice(i,1);
+  }
+  update(product: product): void{
+    //in order to update a product, the parent component will change the input value
+    //and send it to the child component
+    this.inputProduct = product;
+    this.showFormTemplate = true;
 
-    }
-
-    showForm(){
-      this.showFormTemplate = !this.showFormTemplate;
-      return this.showFormTemplate
-    }
-    delete(product:product){
-      let i = this.listProduct.indexOf(product);
-      this.listProduct.splice(i,1);
-    }
+  }
 
 
 }
